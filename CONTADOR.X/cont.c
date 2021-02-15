16e73a38112ea21a85f177f5a35c5cebe15c5f76
agu18044 <78278462+agu18044@users.noreply.github.com>
2021-02-14 23:51:04 -0600

    añadio libreria SPI

---
+++ CONTADOR.X/cont.c
@@ -0,0 +1,58 @@
+/*
+ * File:   CONT.c
+ * Author: Usuario Dell
+ *
+ * Created on 14 de febrero de 2021, 11:44 PM
+ */
+
+#include <xc.h>
+#include <stdint.h>
+#include "SPI.h"
+
+//******************************************************************************
+//  Palabra de cofiguración
+//******************************************************************************
+// CONFIG1
+#pragma config FOSC = EXTRC_NOCLKOUT        // Oscillator Selection bits (XT oscillator: Crystal/resonator on RA6/OSC2/CLKOUT and RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF      // RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = OFF      // Brown Out Reset Selection bits (BOR disabled)
+#pragma config IESO = OFF       // Internal External Switchover bit (Internal/External Switchover mode is disabled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
+#pragma config LVP = OFF        // Low Voltage Programming Enable bit (RB3 pin has digital I/O, HV on MCLR must be used for programming)
+
+// CONFIG2
+#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
+
+//******************************************************************************
+//  Variables
+//******************************************************************************
+#define _XTAL_FREQ 8000000
+
+
+
+//******************************************************************************
+//  Prototipos de funciones
+//******************************************************************************
+void setup(void);
+
+//******************************************************************************
+//  Ciclo principal
+//******************************************************************************
+void main(void) {         
+    setup();
+    while(1){
+    
+    }
+}
+
+//******************************************************************************
+//  Cofiguración
+//******************************************************************************
+void setup(void) {
+    
+    }
